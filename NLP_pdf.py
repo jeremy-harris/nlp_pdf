84bef6141e6d9893de838d017c32c0b063607dee
jeremy-harris <45572590+jeremy-harris@users.noreply.github.com>
2020-11-23 22:16:21 -0500

    update script with stop words and getting ready to do work

--- NLP_pdf.py
+++ NLP_pdf.py
@@ -8,6 +8,14 @@ import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 import spacy
 from spacy.matcher import PhraseMatcher
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+
+import nltk
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+nltk.download('punkt')
+from nltk.tokenize import word_tokenize
 
 #Read in the document. In this case, I'm using the NIST AI meeting transcribed document. 
 filename = 'NIST_AI_transcript.pdf'
@@ -21,13 +29,33 @@ strText = text.decode(encoding)
 #for some reason, the pdf font didn't bring over the letters 'v' or 'y'
 spell = Speller(lang='en')
 spellText = spell(strText)
+spellText = spellText.lower()
 
 #load English language module for nlp
 nlp = spacy.load('en_core_web_sm')
 
+#tokenize the information so that we can work with it in spacy
+text_tokens = word_tokenize(spellText)
+
+#pull out stop words and keep remaining text
+all_stopwords = nlp.Defaults.stop_words
+all_stopwords |= {"as", "if", "cut", "ou", "ant", "oka", "apical", "so", "er", "si", "ans", "ie", "gi",
+	"ha", "es", "ne", "ing", "ide", "ma", "ell", "ling", "ori", "ho", "ops", "ent", "ith", "sa", "ist",
+	"ol", "a.", "yeah", "--", "de", "ill", "ance", "ef", "di", "tr", "ta", "nd", "pri", "ac", "ard", "lea",}
+text_no_stop = [word for word in text_tokens if not word in all_stopwords]
+
+print(text_no_stop)
+
+
+
 #process text with spacy and the English module loaded above
-doc = nlp(spellText)
+#doc = nlp(spellText)
+
+
 
 #####
 # Maybe from here, do a word cloud or some other way to show the sentiment of the document?
-#####
+#wordcloud = WordCloud(stopwords=STOPWORDS).generate(doc)
+#plt.imshow(wordcloud, interpolation='bilinear')
+#plt.axis("off")
+#plot.show()
