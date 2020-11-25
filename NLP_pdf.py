2a6c373b4dccdb8a4578139f80c13cb529fdf313
jeremy-harris <45572590+jeremy-harris@users.noreply.github.com>
2020-11-24 22:28:58 -0500

    have all text cleaned and in df. Ready to begin bag of words and create word cloud.

--- NLP_pdf.py
+++ NLP_pdf.py
@@ -16,7 +16,9 @@ import nltk
 from nltk.corpus import stopwords
 nltk.download('stopwords')
 nltk.download('punkt')
-from nltk.tokenize import word_tokenize
+from nltk.tokenize import word_tokenize, sent_tokenize, RegexpTokenizer
+from nltk.stem import WordNetLemmatizer
+nltk.download('wordnet')
 
 #Read in the document. In this case, I'm using the NIST AI meeting transcribed document. 
 filename = 'NIST_AI_transcript.pdf'
@@ -42,32 +44,39 @@ no_single = re.sub(r'\s+[a-zA-Z]\s+', ' ', no_num)
 #load English language module for nlp
 nlp = spacy.load('en_core_web_sm')
 
-#tokenize the information so that we can work with it in spacy
-text_tokens = word_tokenize(no_single)
-#full_text = nlp(spellText)
-
-
 #had to add in a few extra stop words because of the poor translation from pdf to work and missing letters
 all_stopwords = nlp.Defaults.stop_words
 all_stopwords |= {"as", "if", "cut", "ou", "ant", "oka", "apical", "so", "er", "si", "ans", "ie", "gi",
 	"ha", "es", "ne", "ing", "ide", "ma", "ell", "ling", "ori", "ho", "ops", "ent", "ith", "sa", "ist",
 	"ol", "a.", "yeah", "--", "de", "ill", "ance", "ef", "di", "tr", "ta", "nd", "pri", "ac", "ard", "lea",
 	"pa", "ia", "bod", "da", "yup", "yep", "lot", "et", "ed", "mo", "/", "//", "e.", "la", "lo", "e", "-up",
-	"dif", "gu", "ste", "al", "ere", "'", "."}
+	"dif", "gu", "ste", "al", "ere", "'", ".", "spec", "co", "cult", ""}
+
+#tokenize the information so that we can work with it in spacy
+text_tokens = word_tokenize(no_single)
+#full_text = nlp(spellText)
 
 #remove stop words
 no_stops = [word for word in text_tokens if not word in all_stopwords]
 
 #remove punctuation
-punctuations = ['.', ',', '/', '!', '?', ';', ':', '(',')', '[',']', '-', '_', '%']
+punctuations = ['.', '..', ',', '/', '!', '?', ';', ':', '(',')', '[',']', '-', '_', '%']
 no_punc = [word for word in no_stops if not word in punctuations]
 
-#remove numbers
-def cleanUp(text):
-	output = ' '.join(c for c in text if not c.isdigit())
-	return output
+# lemmatize words
+not_token_data = ' '.join(no_punc)
 
-# lemmatize words?
+lemmatizer = WordNetLemmatizer()
+lemmatized_data = [lemmatizer.lemmatize(word) for word in not_token_data]
+
+# clean data
+clean_data = ''.join(lemmatized_data)
+
+#create dataframe to analyze data
+df = pd.DataFrame([clean_data])
+df.columns = ['words']
+df.index = ['document']
+print(df)
 
 # set frequency threshold
 
